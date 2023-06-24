6d7a27740a463094775192b720b253442b18e635
Vũ Đình Nguyễn <nguyenvu270901@gmail.com>
2023-06-24 22:09:33 +0700

    Update BEv3

--- BE/copy_artifact.sh
+++ BE/copy_artifact.sh
@@ -2,6 +2,9 @@
 sudo mkdir -p /var/www/eyesight.is-tech.vn/server
 sudo mkdir -p /var/www/eyesight.is-tech.vn/server/logs
 sudo cp -r ./Release/net6.0/* /var/www/eyesight.is-tech.vn/server
+sudo supervisorctl stop nguyenvu-eyesight:*
 sudo touch /etc/supervisor/conf.d/nguyenvu.conf
 sudo cp supervisor.conf /etc/supervisor/conf.d/nguyenvu.conf
+sudo supervisorctl update
+sudo supervisorctl start nguyenvu-eyesight:*
 
