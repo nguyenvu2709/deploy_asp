4c1da4563c78059892e6bda9901e4e80f77d9266
Vũ Đình Nguyễn <nguyenvu270901@gmail.com>
2023-06-26 23:49:58 +0700

    Update BEA7

--- FE/copy_artifact.sh
+++ FE/copy_artifact.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo mkdir -p /var/www/eyesight.is-tech.vn/html
 sudo cp -r ./build/* /var/www/eyesight.is-tech.vn/html
-sudo touch /etc/nginx/sites-available/nginx.conf
-sudo cp nginx.conf /etc/nginx/sites-available/nginx.conf
-sudo ln -s  /etc/nginx/sites-available/nginx.conf /etc/nginx/sites-enabled/nginx.conf
+sudo touch /etc/nginx/sites-available/nguyenvu.conf
+sudo cp nginx.conf /etc/nginx/sites-available/nguyenvu.conf
+sudo ln -s  /etc/nginx/sites-available/nguyenvu.conf /etc/nginx/sites-enabled/nguyenvu.conf
